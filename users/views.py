d53b63429c0e9e2f6ad7dcdb9c9d397278a6ee19
HOSHI Seigo <13637027+subsonicsystems@users.noreply.github.com>
2024-08-04 13:16:43 +0900

    ユーザーに姓と名を追加

--- users/views.py
+++ users/views.py
@@ -5,10 +5,12 @@ from http import HTTPStatus
 import requests
 from authlib.integrations.django_oauth2 import ResourceProtector
 from django.http import JsonResponse, HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from dotenv import load_dotenv
 
 from users import validator
+from users.models import User
 
 load_dotenv()
 
@@ -59,6 +61,7 @@ def get_user(request: HttpRequest) -> HttpResponse:
         return HttpResponse(status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
     user_id = request.oauth_token.sub
+    user = get_object_or_404(User, auth0_user_id=user_id)
 
     headers = {
         'Accept': 'application/json',
@@ -73,7 +76,8 @@ def get_user(request: HttpRequest) -> HttpResponse:
     data = response.json()
 
     return JsonResponse({
-        'name': data['name'],
+        'first_name': user.first_name,
+        'last_name': user.last_name,
         'email': data['email']
     })
 
@@ -88,15 +92,20 @@ def update_profile(request: HttpRequest) -> HttpResponse:
     :return: 更新内容
     :rtype: HttpResponse
     """
+    user_id = request.oauth_token.sub
+
+    user = get_object_or_404(User, auth0_user_id=user_id)
+    user.first_name = request.POST['first_name']
+    user.last_name = request.POST['last_name']
+    user.save()
+
     access_token = get_auth0_management_api_access_token()
 
     if access_token is None:
         return HttpResponse(status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
-    user_id = request.oauth_token.sub
-
     payload = json.dumps({
-        'name': request.POST['name'],
+        'name': f'{request.POST['last_name']} {request.POST['first_name']}',
         'email': request.POST['email']
     })
 
